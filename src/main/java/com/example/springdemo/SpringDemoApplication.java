e5970f434dcf3a1fcf60a0ec375ac18ed29c93b0
maatoub <abdenasser.hasnaoui@edu.uiz.ac.ma>
2024-04-13 16:42:54 +0000

    feat: :fire: add microsoft sql server

--- src/main/java/com/example/springdemo/SpringDemoApplication.java
+++ src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -4,14 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.User;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import com.example.springdemo.service.AppService;
 import com.example.springdemo.service.AppServiceImpl;
-import com.example.springdemo.service.ServiceUserDetailsImp;
 
 @SpringBootApplication
 public class SpringDemoApplication {
@@ -25,13 +22,11 @@ public class SpringDemoApplication {
 		return new BCryptPasswordEncoder();
 	}
 
-	
-
-	/******************** inialize BDD  *********************** */
-	//@Bean
+	/******************** inialize BDD *********************** */
+	// @Bean
 	CommandLineRunner commandLineRunner(AppServiceImpl appService) {
 		return args -> {
-			
+
 			appService.addUser("user1", "12345", "12345");
 			appService.addUser("nasser", "12345", "12345");
 			appService.addRole("USER");
